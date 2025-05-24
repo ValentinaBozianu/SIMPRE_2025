bfa6f2269b2932f8620a825b13f6ba2d94fde4e4
ValentinaBozianu <bozianuvalentina21@stud.ase.ro>
2025-05-24 03:35:16 +0300

    flower update crud

--- components/MainPage.jsx
+++
@@ -1,8 +0,0 @@
-// components/MainPage.jsx
-import { useState, useEffect } from "react";
-import styles from "@/styles/FlowerStyles.module.css";
-
-const MainPage = () => {
-};
-
-export default MainPage;
